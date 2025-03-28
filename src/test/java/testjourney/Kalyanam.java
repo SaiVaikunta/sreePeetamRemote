b65b962beb31320597202db02a4fdba261f47a02
sai <saipavan216@gmail.com>
2025-03-28 15:15:32 +0530

    Avirbhava

---
+++ src/test/java/testjourney/Kalyanam.java
@@ -0,0 +1,12 @@
+package testjourney;
+
+public class Kalyanam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Ksheera Sagara Madanam");
+			
+	}
+
+}
